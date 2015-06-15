4b87aa5edd547b8dc03bf9735ea45b2b9e52b94d
Jamieson Warner <jamiesonwarner@gmail.com>
2015-06-15 12:55:36 +0200

    add autotracking example

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,8 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+  $scope.sectorNumber = 3;
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
